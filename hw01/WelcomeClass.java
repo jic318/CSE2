4407ebf3819ee1d3ff4c708d424b5032b5e4dcfe
jic318 <jic318@lehigh.edu>
2014-09-01 14:13:52 +0000

    updated comment

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,19 @@
+//hw01
+//jic318
+//define a class
+public class WelcomeClass{
+    //add main method
+    public static void main(String[] args) {
+        //print the image
+        System.out.println("--------------------");
+        System.out.println("    WelcomeClass!   ");
+        System.out.println("--------------------");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-E--J--K--O--O--O-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("This August my wife and I took an eleven-day, 675-mile bike");
+        System.out.println("tour on our tandem. We circled Lake Ontario");
+    }
+}
