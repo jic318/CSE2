a3fccb8db34e5e376e6caa5e49ad923114bbb2de
jic318 <jic318@lehigh.edu>
2014-10-15 02:50:16 +0000

    updated comment

--- hw06/Roulette.java
+++ hw06/Roulette.java
@@ -5,9 +5,9 @@ public class Roulette {
     //define a method
     public static void main (String[] args){
         //begin
-        int i1=0, i2, countWin, R1, R2, away=0, away1=0, loss=0;
+        int i1=0, i2, countWin, R1, R2, away=0, away1=0, loss=0;//define the initial values
         while (i1<1000) {
-            R1=(int)(Math.random()*38);
+            R1=(int)(Math.random()*38);//define what the player is going to bet
             countWin=0;
             i2=0;
             while (i2<100) {
@@ -15,14 +15,14 @@ public class Roulette {
                 if (R1==R2) {
                     countWin++;
                 }//when the player wins
-                if (countWin==3) {
-                    away++;
-                    break;
-                }//when the player walk away
                 i2++;
+               
                 
             }//end of the loop of 100 times
             
+                if (countWin>=3) {
+                    away++;
+                }//when the player has pofit
                 if (countWin<3&&countWin>0) {
                     away1++;
                 }//when the profit is not suffcient    
@@ -31,7 +31,7 @@ public class Roulette {
                 }//when the player lose
                 i1++;
         }//end of the loop of 1000 times
-        System.out.println("The player walk away with profit for "+away+" times, and walk away with insufficient profit for "+away1+" times and loss for "+loss+" times");
+        System.out.println("The player walk away with profit for "+away+" times, and walk away with insufficient profit for "+away1+" times and lose for "+loss+" times");
         
         
     }//end of method
